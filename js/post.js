a8dfc5e38d14a8fc1d54818dfaf21758538a21c0
SukuC <48251127+SukuC@users.noreply.github.com>
2019-07-07 04:26:28 +0530

    Like,Save,Comment Functionalities Complete

---
+++ js/post.js
@@ -0,0 +1,36 @@
+const editButton = document.getElementById('editButton');
+const saveButton = document.getElementById('saveButton');
+const blogBody = document.getElementById('blogBody');
+
+editButton.onclick = function () {
+    editButton.style.display = "none";
+    saveButton.style.display = "block";
+    const postValue = document.getElementById('blogBody').innerHTML;
+    blogBody.innerHTML = `<textarea id="instantTextArea" style="width: 100%;height: 35rem;font-size: 1.8rem">${postValue}</textarea>`;
+
+};
+
+saveButton.onclick = function () {
+    editButton.style.display = "block";
+    saveButton.style.display = "none";
+    const instantTextArea = document.getElementById('instantTextArea');
+    const value = instantTextArea.value;
+    if (value) {
+        blogBody.innerHTML = `<p id="blogBody">${value}</p>`
+    }
+};
+
+const commentArea = document.getElementById('commentArea');
+const commentHolder = document.getElementById('commentHolder');
+const commentButton = document.getElementById('commentButton');
+
+const comments = [];
+
+commentButton.onclick = function () {
+    const value = commentArea.value;
+
+    const comment = `<div class="comment"><p>${value}</p></div>`;
+    comments.push(comment);
+    commentHolder.innerHTML = comments;
+};
+
